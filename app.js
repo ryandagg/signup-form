c1b56712b7c3155b1323a960076b4e98218954bb
ryandagg <ryan.dagg@gmail.com>
2014-07-22 15:05:11 -0600

    Finished. Post/Redirect/Get implemented.

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.get('/success', function(req, res) {
+	res.send("<h1>Success!</h1>")
+})
+
+var lastSubmission;
+
+app.post("/formsubmit", function(req, res) {
+	lastSumbission = req.body;
+
+	res.redirect("/success");
+})
+
 var server = app.listen(8683, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
